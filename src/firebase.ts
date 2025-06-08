3952dbe6bcfedec5e04977b2b2b258271a1ed068
omarraf <omarrafiqq@gmail.com>
2025-06-07 20:15:04 -0500

    update

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,13 +7,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCbd3OYYzB89oGPTa2DhpVYbRb35PKfiY4",
-  authDomain: "time-ea070.firebaseapp.com",
-  projectId: "time-ea070",
-  storageBucket: "time-ea070.firebasestorage.app",
-  messagingSenderId: "141515756799",
-  appId: "1:141515756799:web:dfcb09e8c2bf326e60e8ca",
-  measurementId: "G-9ZKFSNZN89"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
